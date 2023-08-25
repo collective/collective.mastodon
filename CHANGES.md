28833ff3a7278ff0dd703cd2d60c04b5cecaa905
Érico Andrei <ericof@gmail.com>
2023-08-25 16:52:13 -0300

    Back to development: 1.0.0a2

--- CHANGES.md
+++ CHANGES.md
@@ -1,1 +1,7 @@
+## 1.0.0a2 (unreleased)
+
+
+- Nothing changed yet.
+
+
 ## 1.0.0a1 (2023-08-25)
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = f"""
 
 setup(
     name="collective.mastodon",
-    version="1.0.0a1",
+    version="1.0.0a2.dev0",
     description="Mastodon integration for Plone.",
     long_description=long_description,
     long_description_content_type="text/markdown",
