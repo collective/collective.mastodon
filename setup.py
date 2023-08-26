1886f98f17f2e172bba971b6d0edc036b146f980
Érico Andrei <ericof@gmail.com>
2023-08-26 13:25:30 -0300

    Back to development: 1.0.0a3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = f"""
 
 setup(
     name="collective.mastodon",
-    version="1.0.0a2",
+    version="1.0.0a3.dev0",
     description="Mastodon integration for Plone.",
     long_description=long_description,
     long_description_content_type="text/markdown",
